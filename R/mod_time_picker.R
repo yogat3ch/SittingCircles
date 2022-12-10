19109424b6ea3a330691796bc5028c432e4cba45
Stephen Holsenbeck <stephen@virgalabs.io>
2022-12-10 06:50:23 -0500

    time_picker: add_week, duplicate/remove select rows, default duration

--- R/mod_time_picker.R
+++ R/mod_time_picker.R
@@ -15,7 +15,27 @@ mod_time_picker_ui <- function(id){
       title = "Preferred times to meditate",
       uiOutput(ns("time_picker")),
       footer = fluidRow(
-        col_6(),
+        col_2(
+          sliderInput(
+            ns("duration"),
+            "Default Duration (min)",
+            min = 0,
+            max = 3 * 60,
+            value = 20,
+            step = 5
+          )
+        ),
+        col_2(
+          
+        ),
+        col_2(
+          bs4Dash::actionButton(
+            ns("add_week"),
+            "Add full week of last entered times",
+            icon = shiny::icon("calendar"),
+            width = "100%"
+          )
+        ),
         col_2(
           bs4Dash::actionButton(
             ns("subtract_time"),
@@ -49,7 +69,7 @@ ui_picker <- function(id_suffix = 1, day = "Monday", begin = NULL, end = NULL, s
   ns <- session$ns
   ui_row(
     box = FALSE,
-    col_4(
+    col_2(
       shinyWidgets::virtualSelectInput(
         inputId = ns(paste0("day", id_suffix)),
         label = "Day of week",
@@ -73,6 +93,22 @@ ui_picker <- function(id_suffix = 1, day = "Monday", begin = NULL, end = NULL, s
                            minute.steps = 5,
                            value = end) |> 
         htmltools::tagAppendAttributes(class = "time_input", .cssSelector = ".input-group > input")
+    ),
+    col_2(
+      bs4Dash::actionButton(
+        ns(paste0("rm", id_suffix)),
+        "",
+        icon = shiny::icon("minus"),
+        width = "35px"
+      ) |> 
+        tagAppendAttributes(style = "width:35px;height:36px;margin-top:24px"),
+      bs4Dash::actionButton(
+        ns(paste0("add", id_suffix)),
+        "",
+        icon = shiny::icon("plus"),
+        width = "35px"
+      ) |> 
+        tagAppendAttributes(style = "width:35px;height:36px;margin-top:24px")
     )
   )
 }
@@ -87,6 +123,7 @@ ui_picker <- function(id_suffix = 1, day = "Monday", begin = NULL, end = NULL, s
 time_handler <- function(x) {
   UseMethod("time_handler")
 }
+
 #' @export
 time_handler.data.frame <- function(x) {
   slider::slide(x, \(.vars) {
@@ -110,12 +147,21 @@ time_handler.character <- function(x) {
 }
 .time_segs <- c("day", "begin", "end")
 
-time_rows <- function(x = 1) {
+time_rows <- function(x = 1, day = "Monday", duration, begin, end) {
   time <- lubridate::today(tzone = "UTC")
   lubridate::hour(time) <- 0
   lubridate::minute(time) <- 0
   lubridate::second(time) <- 1
-  out <- tibble::tibble_row(day = "Monday", begin = time, end = time)
+  if (missing(begin))
+    begin <- time
+  if (missing(end)) {
+    if (!missing(duration))
+      time <- time + lubridate::minutes(duration)
+    end <- time
+  }
+    
+  
+  out <- tibble::tibble(day = day, begin = begin, end = end)
   if (x > 1)
     out <- dplyr::bind_rows(
       out,
@@ -174,18 +220,57 @@ mod_time_picker_server <- function(id){
       )
       
     })
+    
     observeEvent(input$add_time, {
       active$times_df(
         dplyr::bind_rows(
           ui_picker_gatherer(max_inputs = nrow(active$times_df())),
-          time_rows()
+          time_rows(duration = input$duration)
         )
       )
-    })
+    }, priority = 2)
+    observeEvent(input$add_week, {
+      len <- nrow(active$times_df())
+      times <- ui_picker_gatherer(max_inputs = len)
+      days <- stringr::str_subset(UU::week_factor(), times[len,]$day, negate = TRUE)
+      active$times_df(
+        dplyr::bind_rows(
+          times,
+          time_rows(
+            day = days,
+            begin = times[len,]$begin,
+            end = times[len,]$end
+          )
+        )
+      )
+    }, priority = 2)
     observeEvent(input$subtract_time, {
       active$times_df(
         ui_picker_gatherer(max_inputs = nrow(active$times_df()) - 1)
       )
+    }, priority = 2)
+    
+    time_env <- environment()
+    observeEvent(c(input$add_week, input$add_time, input$subtract_time), {
+      len <- nrow(active$times_df())
+      input_chr <- purrr::walk(1:len, ~{
+        inputs <- paste0("input$", c("rm", "add"), .x)
+        observeEvent(rlang::parse_expr(inputs[1]), rlang::expr({
+          active$times_df(
+            active$times_df()[-!!.x, ]
+          )
+        }), event.env = time_env, event.quoted = TRUE, handler.env = time_env, handler.quoted = TRUE)
+        observeEvent(rlang::parse_expr(inputs[2]), rlang::expr({
+          active$times_df(
+            dplyr::add_row(
+              active$times_df(),
+              .after = !!.x,
+              active$times_df()[!!.x, ]
+            )
+          )
+        }), event.env = time_env, event.quoted = TRUE, handler.env = time_env, handler.quoted = TRUE)
+      })
+      
     })
     
     output$time_picker <- renderUI({
